4171a61f6cece2a730262b0353d1f113027989e0
ricardus <ricardus1999@pusan.ac.kr>
2022-05-18 10:01:27 +0900

    'team2'

---
+++ include/node.h
@@ -0,0 +1,13 @@
+
+de {
+
+    char* data;
+struct Node {
+    struct Node* prev;
+
+    struct Node* next;
+
+s;
+
+
+typedef struct Node Node;i
---
+++ libs/linkedlist.c
@@ -0,0 +1,8 @@
+
+static Nodede _head = NULL;
+
+static Node* _tail = NULL;
+
+static Node* _cur_node = NULL;
+
+
