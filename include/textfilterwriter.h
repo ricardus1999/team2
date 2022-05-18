359189ce930c240a23cf110850df51e04c6fa458
ricardus1999 <104752701+ricardus1999@users.noreply.github.com>
2022-05-18 10:13:44 +0900

    Update textfilterwriter.h

--- include/textfilterwriter.h
+++ include/textfilterwriter.h
@@ -1,10 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
 
 #define MAX_TITLE_SIZE 50
 
 void create_music_titles(FILE* stream);
 
 void read_file(char* file_name);
-n:wq
 
 void write_file(char* file_name);
+
