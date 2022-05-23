3d6060325eecb4e621f776b4177094459a1b2795
ricardus1999 <104752701+ricardus1999@users.noreply.github.com>
2022-05-23 09:33:22 +0900

    Update textfilterwriter.c

---
+++ libs/textfilterwriter.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linkedlist.h"
+#include "textfilewriter.h"
+
+void create_music_titles(FILE* stream){
+
+}
+
+void read_file(char* file_name){
+
+}
+
+void write_file(char* file_name){
+
+}
