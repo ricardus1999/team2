a0a759819ec26cf839cb8f03d71b0457f85d6815
ricardus1999 <104752701+ricardus1999@users.noreply.github.com>
2022-05-23 09:34:11 +0900

    Update Player.c

---
+++ src/Player.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linkedlist.h"
+#include "textfilewriter.h"
+
+void read_command(char** cmds);
+
+int main(){
+    
+}
+
+void read_command(char** cmds) {
+    
+}
